f612770f1231eaa81c421517dae2562922cc3d96
Vaibhav <Vaibhava@VAIBHAVA.emids.local>
2015-11-03 18:11:58 +0530

    First Commit

---
+++ Home.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace JiraMetrics
+{
+    public partial class Home : System.Web.UI.Page
+    {
+        string path;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {										
+            if (FileUpload1.HasFile)
+            {
+                path = string.Concat(Server.MapPath("~/Content/" + FileUpload1.FileName));
+                FileUpload1.SaveAs(path); //DefaultConnection
+            }
+        }
+    }
+}
